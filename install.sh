c8aca17ac4c107098382fa847148d62ccf149798
H Xu <xuhdev@gmail.com>
2011-06-01 10:23:59 +0800

    Initial commit.

---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+INSTALL_DIRS=(\
+  '/usr/lib64/nautilus/extensions-3.0/python' \
+  '/usr/lib64/nautilus/extensions-2.0/python' \
+  '/usr/lib/nautilus/extensions-3.0/python' \
+  '/usr/lib/nautilus/extensions-2.0/python' \
+  "$HOME/.nautilus/python-extensions" )
+
+for ONE_INSTALL_DIR in ${INSTALL_DIRS[@]};
+do
+  # try to make the directory if not exist
+  if [ ! -e $ONE_INSTALL_DIR ]
+  then
+    mkdir -p $ONE_INSTALL_DIR >/dev/null 2>&1 
+
+    # if failed to create the directory, then let's turn to next directory in
+    # the list
+    if [ $? != 0 ]
+    then
+      continue
+    fi
+  fi
+
+  # if we are able to write to the directory, try to copy nautilusvim.py to
+  # it.
+  if [ -d $ONE_INSTALL_DIR ] && [ -w $ONE_INSTALL_DIR ]
+  then
+    cp nautilusvim.py "$ONE_INSTALL_DIR/"
+
+    echo "nautilus-py-vim has been successfully installed to $ONE_INSTALL_DIR"
+
+    exit
+  fi
+done
+
+# if we are here, the only reason is that the installation failed
+echo "Failed to install nautilus-py-vim. You may try to install it manually."
+echo "See README for more detailed."
+
+# vim73: cc=78
+# vim: ts=2 sw=2 et tw=78
---
+++ nautilusvim.py
@@ -0,0 +1,75 @@
+# File: nautilusvim.py
+#
+# This file is the major part of nautilus-py-vim.
+#
+# By Hong Xu
+
+import os
+import nautilus
+import gconf
+
+
+class NautilusVimExtension(nautilus.MenuProvider):
+
+    def __init__(self):
+        self.client = gconf.client_get_default()
+
+    #edit with single gvim
+    def menu_activate_cb_single(self, menu, files):
+        cmd_string = 'gvim '
+        for afile in files:
+            cmd_string += "'" + afile.get_location().get_path() + "' "
+
+        os.system(cmd_string)
+
+    # edit with mutli gvim
+    def menu_activate_cb_multi(self, menu, files):
+        for afile in files:
+            os.system('gvim ' + "'" + afile.get_location().get_path() + "'")
+
+    # diff with gvim
+    def menu_activate_cb_diff(self, menu, files):
+        cmd_string = 'gvim -d '
+        for afile in files:
+            cmd_string += "'" + afile.get_location().get_path() + "' "
+        
+        os.system(cmd_string)
+
+    def get_file_items(self, window, files):
+        items = []
+
+        if len(files) == 1:
+            new_item = nautilus.MenuItem(
+                'NautilusPython::nautilusvim_file_item',
+                'Edit with gVim',
+                'Edit with gVim Editor')
+            new_item.connect('activate', self.menu_activate_cb_single, files)
+            items.append(new_item)
+
+        elif len(files) > 1:
+            new_item = nautilus.MenuItem(
+                'NautilusPython::nautilusvim_single_file_item',
+                'Edit with a Single gVim',
+                'Edit with a Single gVim Editor')
+            new_item.connect('activate', self.menu_activate_cb_single, files)
+            items.append(new_item)
+
+            new_item = nautilus.MenuItem(
+                'NautilusPython::nautilusvim_multi_file_item',
+                'Edit with Multi gVim',
+                'Edit with Mutli gVim Editors')
+            new_item.connect('activate', self.menu_activate_cb_multi, files)
+            items.append(new_item)
+
+            new_item = nautilus.MenuItem(
+                'NautilusPython::nautilusvim_diff_file_item',
+                'Diff with gVim',
+                'Diff with gVim')
+            new_item.connect('activate', self.menu_activate_cb_diff, files)
+            items.append(new_item)
+
+
+        return items
+
+# vim703: cc=78
+# vim: et tw=78 sw=4 ts=4
