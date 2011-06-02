26194dad00757336fbca57d9a8b73debac8b4113
H Xu <xuhdev@gmail.com>
2011-06-02 08:55:10 +0800

    Fix some problem in the install script.

--- install.sh
+++ install.sh
@@ -17,19 +17,6 @@ INSTALL_DIRS=(\
 
 for ONE_INSTALL_DIR in ${INSTALL_DIRS[@]};
 do
-  # try to make the directory if not exist
-  if [ ! -e $ONE_INSTALL_DIR ]
-  then
-    mkdir -p $ONE_INSTALL_DIR >/dev/null 2>&1 
-
-    # if failed to create the directory, then let's turn to next directory in
-    # the list
-    if [ $? != 0 ]
-    then
-      continue
-    fi
-  fi
-
   # if we are able to write to the directory, try to copy nautilusvim.py to
   # it.
   if [ -d $ONE_INSTALL_DIR ] && [ -w $ONE_INSTALL_DIR ]
@@ -37,14 +24,16 @@ do
     cp nautilusvim.py "$ONE_INSTALL_DIR/"
 
     echo "nautilus-py-vim has been successfully installed to $ONE_INSTALL_DIR"
+    echo "Restart nautilus to complete the installation."
 
     exit
   fi
 done
 
 # if we are here, the only reason is that the installation failed
-echo "Failed to install nautilus-py-vim. You may try to install it manually."
-echo "See README for more detailed."
+echo "Failed to install nautilus-py-vim. You may try to install it manually"
+echo "by copying nautilusvim.py to ~/.nautilus/python-extensions/ and then"\
+  "restart nautilus."
 
 # vim73: cc=78
 # vim: ts=2 sw=2 et tw=78
