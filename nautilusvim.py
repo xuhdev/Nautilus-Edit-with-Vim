5287d91919fbf9db17d0f45dac0508b3b1349023
xuhong <xuhong@localhost.localdomain>
2011-06-03 02:34:36 -0700

    Makes it possible for user to customize their gvim command and gvimdiff command.

--- nautilusvim.py
+++ nautilusvim.py
@@ -9,31 +9,69 @@
 import os
 import nautilus
 import gconf
+import ConfigParser
 
 
 class NautilusVimExtension(nautilus.MenuProvider):
 
     def __init__(self):
         self.client = gconf.client_get_default()
+        self.read_conf_file()
+
+    # read config file and set values
+    def read_conf_file(self):
+
+        # first set default value
+        self.gvim_cmd = 'gvim'
+        self.gvimdiff_cmd = 'gvim -d'
+
+        home_dir = os.getenv('HOME', '/');
+
+        conf_file = None
+
+        for a_file in [
+                home_dir + '/.nautilusvim.conf',
+                home_dir + '/.nautilus/.nautilusvim.conf',
+                '/etc/nautilusvim.conf']:
+            if os.path.isfile(a_file) and os.access(a_file, os.R_OK):
+                conf_file = a_file
+                break
+
+        if conf_file == None:
+            return
+
+        cf = ConfigParser.ConfigParser(False)
+
+        cf.read(conf_file)
+
+        tmpval = cf.get('cmds', 'gvim')
+        if tmpval != None:
+            self.gvim_cmd = tmpval
+
+        tmpval = cf.get('cmds', 'gvimdiff')
+        if tmpval != None:
+            self.gvimdiff_cmd = tmpval
+
 
     #edit with single gvim
     def menu_activate_cb_single(self, menu, files):
-        cmd_string = 'gvim '
+        cmd_string = self.gvim_cmd
         for afile in files:
-            cmd_string += "'" + afile.get_location().get_path() + "' "
+            cmd_string += " '" + afile.get_location().get_path() + "'"
 
         os.system(cmd_string)
 
     # edit with mutli gvim
     def menu_activate_cb_multi(self, menu, files):
         for afile in files:
-            os.system('gvim ' + "'" + afile.get_location().get_path() + "'")
+            os.system(self.gvim_cmd + " '" +
+                    afile.get_location().get_path() + "'")
 
     # diff with gvim
     def menu_activate_cb_diff(self, menu, files):
-        cmd_string = 'gvim -d '
+        cmd_string = self.gvimdiff_cmd
         for afile in files:
-            cmd_string += "'" + afile.get_location().get_path() + "' "
+            cmd_string += " '" + afile.get_location().get_path() + "'"
         
         os.system(cmd_string)
 
