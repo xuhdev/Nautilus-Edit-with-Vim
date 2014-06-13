40fb95e035603d4e31328e16fe9ed39beae82018
Hong Xu <hong@topbug.net>
2014-06-13 14:10:35 -0700

    Add license.

--- nautilusvim.py
+++ nautilusvim.py
@@ -1,10 +1,19 @@
 # File: nautilusvim.py
 #
-# version 0.3
-# by H Xu
-#
-# This file is the major part of nautilus-edit-with-vim.
+# This file is part of Nautilus-Edit-with-Vim.
 
+# Nautilus-Edit-with-Vim is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+
+# Nautilus-Edit-with-Vim is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+
+# You should have received a copy of the GNU General Public License along with
+# Nautilus-Edit-with-Vim.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
 from gi.repository import Nautilus, GObject
